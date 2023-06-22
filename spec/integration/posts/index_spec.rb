a366f9636ada42b0bf0d69a1be6c02d5967fce0f
atok624 <84607674+atok624@users.noreply.github.com>
2023-06-22 09:46:25 +0000

    Fix rubocop error

--- spec/integration/posts/index_spec.rb
+++ spec/integration/posts/index_spec.rb
@@ -5,7 +5,7 @@ require 'capybara/rspec'
 RSpec.feature 'User index page' do
   let(:user) { FactoryBot.create(:user) }
   let(:postClick) { FactoryBot.create(:post, text: 'Click me', title: 'First post', author: user) }
-  let!(:posts) { FactoryBot.create_list(:post, 1, author: user,) }
+  let!(:posts) { FactoryBot.create_list(:post, 1, author: user) }
 
   it 'displays the correct information' do
     visit user_posts_path(user)
